c0d1548d6e276d8ab5f63a0ea762d771dab3ee8a
OsirisJr16 <mintsanavalonajunior@example.com>
2025-03-25 16:22:26 +0300

    folder architecture

---
+++ src/layout/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Navbar } from "./navbar";
+import { Footer } from "./footer";
+export const MainLayout = () => {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  );
+};
