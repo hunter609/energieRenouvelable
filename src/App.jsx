3152946a9568c335ee957ead2167a492a1d7fe30
Andi <coconuthunted69@gmail.com>
2025-03-25 16:07:48 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div>
+      <h1 class="text-3xl font-bold underline text-black text-center p-4">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+export default App
