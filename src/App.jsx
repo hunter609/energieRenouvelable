495ef6ded3b88a7c74ef946b23d3431a93e64d09
Andi <coconuthunted69@gmail.com>
2025-03-25 16:57:05 +0300

    Add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+import { MainLayout } from "./layout"
+
 const App = () => {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline text-black text-center p-4">
-        Hello world!
-      </h1>
-    </div>
+    <>
+      <MainLayout />
+    </>
   )
 }
 export default App
