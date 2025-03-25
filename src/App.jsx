319e8f9f6ecf60c46ff5f4181447d70124f0d69c
ahna <herinnyaina.andriam@gmail.com>
2025-03-25 17:14:57 +0300

    fix bug aina

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import Partenaires from "./sections/partenaires"
+
 const App = () => {
   return (
     <div>
       <h1 class="text-3xl font-bold underline text-black text-center p-4">
         Hello world!
       </h1>
+      <Partenaires/>
     </div>
   )
 }
---
+++ src/sections/partenaires.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Partenaires() {
+  return (
+    <>
+        <div className="bg-gray-100 py-10">
+            <div className="container mx-auto text-center">
+                <h2 className="text-2xl font-semibold mb-6">Collaborations and Parteners</h2>
+                <div className="d-flex justify-center items-center space-x-3">
+                    <img src="" alt="logo-parteners" className="h-12" />
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
