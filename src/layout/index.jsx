79bba7b7e292f8a006d8f5402b83419a46b1026c
OsirisJr16 <mintsanavalonajunior@example.com>
2025-03-25 17:42:14 +0300

    fix

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { Navbar } from "./navbar";
 import { Footer } from "./footer";
-import { ReasonToChooseUs } from "../sections/ReasonToChooseUs";
 export const MainLayout = () => {
   return (
     <>
       <Navbar />
-      <ReasonToChooseUs/>
       <Footer />
     </>
   );
